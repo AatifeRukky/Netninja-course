f8e8cc03d18c6c2e561da5042eb11fcc828870f9
Aatife Rukky <iyanuoluwaaatife@gmail.com>
2024-05-28 02:19:08 +0100

    push

--- Javascript course/sandbox.js
+++ Javascript course/sandbox.js
@@ -2,7 +2,7 @@
 /*console.log(1);
 console.log(2); */
 
-let age = 25;
+/*let age = 25;
 let year = 2019;
 console.log(age,year);
 
@@ -87,7 +87,7 @@ console.log(result);*/
 //console.log(5 / 'hallo');
 
 //template strings
-const title = 'Best reads of 2019';
+/*const title = 'Best reads of 2019';
 const author = 'Mario';
 const likes = 30;
 
@@ -103,9 +103,9 @@ let html =`
 <p>By ${author}</p>
 <span>has ${likes} likes </span>
 `;
-console.log(html);8
+console.log(html);8 
 
-let ninjas = ['rukky', 'ivor', 'ebrus'];
+let ninjas = ['rukky', 'ivor', 'ebrus'];*/
 //ninjas[1] = 'uncle';
 //console.log(ninjas [1])
 
@@ -118,4 +118,97 @@ let ninjas = ['rukky', 'ivor', 'ebrus'];
   //let result = ninjas.concat('Tega');
   //let result = ninjas.push('tega');
   
-     console.log(result);
+  //   console.log(result);
+
+  /*let email = ('rukkyaatife@gmail.com');
+
+   let result = email.includes('@');
+
+   console.log(result); */
+
+   //let age = 23;
+   // console.log( age == 23);
+     //console.log( age == 25);
+     /* console.log(age != 23);
+       console.log(age > 20);
+        console.log(age < 20);
+        console.log(age >= 23);
+        console.log(age <= 23)*/
+
+        // let name =[ 'rukky'];
+        // console.log(name == 'rukky'); //abstract equality loose equlity
+         //console.log(name != 'Rukky');
+
+        /* console.log(age == 23)
+         console.log(age =='23')
+         console.log(age != '23')
+
+         //strict comparison
+
+         console.log( age === 23)
+         console.log(age === '23')
+         console.log(age !== '23')*/
+
+        /* let score = '100';
+          
+         score = Number(score);
+         console.log( score + 1);
+
+         let age = 20;
+         age = String(age);
+         console.log(age + 3);
+
+         console.log(typeof age)*/
+
+       //  let result =Boolean(100);
+         //console.log(result, typeof result);
+
+ //for loops
+ // for(let i = 0; i < 5; i++){
+ //   console.log('in loop', i);
+ // }
+  //console.log('loop finished');
+  /*let names = ['rukky', 'ivor', 'ebrus', 'esther', 'tega', 'divine'];
+
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] == 'tega') {
+      break;
+    } else {
+      console.log(names[i]);
+    }
+  }*/
+/*let names = [ 'rukky', 'ivor', 'ebrus', 'esther', 'tega', 'divine'];
+
+ for(let i = 0; i < names.length;  i++ ) {
+  if (names[i] == 'tega') {
+    break;
+  } else {
+    console.log(names[i]);
+  }
+ }*/
+  
+/* let name = [ 'sam' , 'larry' , 'rhoda'];
+ for( i = 0; i < name.length; i++){
+  console.log(name[i])
+ }*/
+
+/* let i =  6
+ do{console.log(i);
+i++;
+ } while( i < 6 );*/
+
+  let password = ('rukky@pa');
+
+  if(password.length >= 10 && password.includes('@'))
+    {
+    console.log("that's a pretty strong password")} 
+    else if(password.length >= 8 || password.includes('@')) {
+    console.log('this password is long enough')
+  }
+  else if(password.length < 8) 
+    { console.log('incomplete password')}
+
+  let user = false;
+  if (!user) {
+    console.log('You need to be logged in to continue');
+  }
