5945f68dacee34a4499eebb12e33d899bc33b8ce
Aatife Rukky <iyanuoluwaaatife@gmail.com>
2024-07-19 13:49:47 +0100

    Comparison added

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 // Randomly testing declaring variables 
 let user = 'Rukky' , message = 'Welcome to Mario club';
 let hello = message;
-alert(hello);
+//alert(hello);
 
 let _ = 6;
 let $ = 16;
 console.log(_ + $);
-alert(_ + $);
+//alert(_ + $);
 
 const myBirthday = '01.12.2000';
 
@@ -17,10 +17,37 @@ console.log(age)
 let name = 'John';
 
 let admin = name;
-alert(admin)
+//alert(admin)
 
 let planetName = 'earth';
 let currentUserName = 'Rukky';
 
 const string = "The revolution will not be televised ";
 console.log(string);
+
+function troubleshooting(){
+	const a = 1;
+	const b = 1;
+
+	let result;
+
+	result = a + b;
+
+	return result;
+}
+const ivor = troubleshooting()
+
+console.log(ivor);
+
+alert(59 != 34);
+
+let answer = 4 > 6;
+console.log(answer);
+
+console.log(`Glow` > `Glee`);
+
+let hour = 10;
+let isWeekend = true;
+if (hour < 11 || hour > 19 || isWeekend) {
+	alert ('The office is closed');
+}
